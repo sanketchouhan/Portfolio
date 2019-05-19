78e51f6299f2f897addaef80ee1ce2fcd91b63a0
Sanket Chouhan <sanket.chouhan555@gmail.com>
2019-05-19 22:31:43 +0530

    full screen nav

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -16,9 +16,45 @@ $(function () {
     TweenMax.to(".content .hireMe", .5, { delay: 3, transform: "translateY(0)", opacity: 1 });
     TweenMax.to(".content .explore", .5, { delay: 3.3, transform: "translateY(0)", opacity: 1 });
 
-    $(".navbar-toggler").click(function () {
-        $(".navbar-toggler").toggleClass('change');
-    })
+    // $(".navbar-toggler").click(function () {
+    //     $(".navbar-toggler").toggleClass('change');
+    // })
 
+    // $(window).on("scroll", function () {
+    //     checkTop()
+    // });
+
+    $(".menu-btn").click(function () {
+        console.log($(window).outerWidth());
+
+        if ($(window).width() > 600) {
+            $(".menu").animate({ height: "100vh", width: "90vw" });
+            $(".menu-overlay").animate({ height: "110vh", width: "90vw" });
+
+            $(".mediaIcons").animate({ height: "60vh", width: "60vw" });
+            $(".mediaIcons-overlay").animate({ height: "70vh", width: "60vw" });
+        }
+        else {
+            $(".menu").animate({ height: "100vh", width: "110vw" });
+            $(".menu-overlay").animate({ height: "110vh", width: "110vw" });
+
+            $(".mediaIcons").animate({ height: "60vh", width: "90vw" });
+            $(".mediaIcons-overlay").animate({ height: "70vh", width: "90vw" });
+        }
+
+        $(".bg").css("display", "block");
+        $(".bg").animate({ opacity: "1" });
+    });
+    $(".bg").click(function () {
+        console.log("click bg working");
+        $(".menu").css({ height: "0vh", width: "0vw" });
+        $(".menu-overlay").css({ height: "0vh", width: "0vw" });
+
+        $(".mediaIcons").css({ height: "0vh", width: "0vw" });
+        $(".mediaIcons-overlay").css({ height: "0vh", width: "0vw" });
+
+        $(".bg").css("display", "none");
+        $(".bg").css("opacity", "0");
+    });
 
 });
